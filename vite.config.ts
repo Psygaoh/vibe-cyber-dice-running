8c157e96d405521cc679084a761ae729eb48495e
Florian Ville <Florian.ville63@gmail.com>
2025-03-29 02:15:28 +0100

    fixing ?

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/vibe-cyber-dice-running/' : '/',
+  plugins: [react(), tailwindcss()]
 })
